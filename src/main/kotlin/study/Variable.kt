ff8ddf31c844ccae60e0c18c3e76f02c6539ecea
ahin0422 <ahinahn0422@gmail.com>
2026-06-22 10:19:48 +0900

    start

---
+++ src/main/kotlin/study/Variable.kt
@@ -0,0 +1,8 @@
+package study
+
+class Variable { // 변수 설정
+    val name = "name" // String / val 변경 불가능
+    val age = 20 // Int
+    val weight = 15.5 // Double
+    var say = "hello" // var = 변경 가능
+}
